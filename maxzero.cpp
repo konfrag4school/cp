8c3c1000d33e855d8dedc6f1658c318b4e22701b
Konstantinos Fragkoulis <147519181+konfrag4school@users.noreply.github.com>
2023-10-31 14:54:56 +0000

    Initial commit

---
+++ maxzero.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <malloc.h>
+#include <unordered_map>
+
+int main() {
+    // freopen("maxzero.in", "r", stdin);
+    // freopen("maxzero.out", "w", stdout);
+    int n;
+    scanf("%d", &n);
+    int *a = (int *)calloc(sizeof(int), n+5);
+
+    a[0] = 0;
+    for(int i = 1; i <= n; ++i) {
+        scanf("%d", &a[i]);
+        a[i] += a[i-1];
+    }
+
+    int ans = 0;
+    std::unordered_map<int, int> m;
+    for(int j = 1; j <= n+1; ++j) {
+        if(m[a[j]] != 0) {
+            ans = std::max(ans, j-m[a[j]]);
+        }
+        if(m[a[j]] == 0) {
+            m[a[j]] = j;
+        }
+    }
+
+    printf("%d\n", ans);
+}
