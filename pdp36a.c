2cf8eac854acff0c2f8b85d47958385d510d19dc
Konstantinos Fragkoulis <147519181+konfrag4school@users.noreply.github.com>
2023-12-05 13:58:45 +0000

    Solution

--- pdp36a.c
+++ pdp36a.c
@@ -1,19 +1,37 @@
-#include <stdio.h>
+#include <cstdio>
 #include <malloc.h>
+#include <stack>
 
 int main() {
-    int n, m;
+    freopen("olivetrees.in", "r", stdin);
+    freopen("olivetrees.out", "w", stdout);
+    int n, m, top, ans = 0;
     scanf("%d %d", &n, &m);
 
-    int *a = (int *)calloc(n, sizeof(int));
-    for(int i = 0; i < m; ++i) {
+    int *a = (int *) calloc(m+5, sizeof(int));
+
+    a[0] = 0;
+    a[m+1] = 0;
+    for(int i = 1; i <= m; ++i) {
         scanf("%d", &a[i]);
         a[i] = n - a[i];
     }
 
-    for(int i = 0; i < m; ++i) {
-        printf("%d", a[i]);
+    std::stack <int> s;
+    s.push(0);
+    for(int i = 1; i <= m+1; ++i) {
+        if(a[s.top()] < a[i]) {
+            s.push(i);
+        } else {
+            while(a[s.top()] > a[i]) {
+                top = s.top();
+                s.pop();
+                ans = std::max(ans, (i-s.top()-1)*a[top]);
+            }
+            s.push(i);
+        }
     }
+    printf("%d\n", ans);
 
     return 0;
 }
